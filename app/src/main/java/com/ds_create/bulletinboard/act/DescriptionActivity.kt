db7672fc6276966207cc49aa8a64cb42bae5904b
DmitrySlv <webmaestros@yandex.ru>
2021-12-06 17:39:00 +0400

    Add DescriptionActivity

---
+++ app/src/main/java/com/ds_create/bulletinboard/act/DescriptionActivity.kt
@@ -0,0 +1,15 @@
+package com.ds_create.bulletinboard.act
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ds_create.bulletinboard.databinding.ActivityDescriptionBinding
+
+class DescriptionActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDescriptionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDescriptionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
