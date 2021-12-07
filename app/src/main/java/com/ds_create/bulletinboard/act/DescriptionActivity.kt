a2c4ce1d3468d21100e4b9c4d8c313e333377ada
DmitrySlv <webmaestros@yandex.ru>
2021-12-07 16:45:33 +0400

    Filling in the text part of the ad

--- app/src/main/java/com/ds_create/bulletinboard/act/DescriptionActivity.kt
+++ app/src/main/java/com/ds_create/bulletinboard/act/DescriptionActivity.kt
@@ -3,6 +3,7 @@ package com.ds_create.bulletinboard.act
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ds_create.bulletinboard.R
 import com.ds_create.bulletinboard.adapters.ImageAdapter
 import com.ds_create.bulletinboard.databinding.ActivityDescriptionBinding
 import com.ds_create.bulletinboard.model.Ad
@@ -32,7 +33,28 @@ class DescriptionActivity : AppCompatActivity() {
 
     private fun getIntentFromMainAct() {
         val ad = intent.getSerializableExtra(AD) as Ad
+        updateUI(ad)
+    }
+
+    private fun updateUI(ad: Ad) {
         fillImageArray(ad)
+        fillTextViews(ad)
+    }
+
+    private fun fillTextViews(ad: Ad) = with(binding) {
+        tvTitle.text = ad.title
+        tvDescription.text = ad.description
+        tvPrice.text = ad.price
+        tvTel.text = ad.tel
+        tvCountry.text = ad.country
+        tvCity.text = ad.city
+        tvIndex.text = ad.index
+        tvWithSend.text = isWithSend(ad.withSend.toBoolean())
+    }
+
+    private fun isWithSend(withSend: Boolean): String {
+        return if (withSend) getString(R.string.desc_act_with_send)
+        else getString(R.string.desc_act_without_send_boolean)
     }
 
     private fun fillImageArray(ad: Ad) {
